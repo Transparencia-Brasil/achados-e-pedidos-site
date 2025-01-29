2d826d4bafd11fb51e4a52b105c1cac9cdb488be
Marcelo Junior <marcelojunior.tech@gmail.com>
2025-01-29 11:23:37 -0300

    FIX: Request header field authorization is not allowed by Access-Control-Allow-Headers in preflight response.

--- webroot/get_log.php
+++ webroot/get_log.php
@@ -6,6 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header("Access-Control-Allow-Origin: * ");
     header('Access-Control-Allow-Credentials: true');
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: *");
     
     exit(0);
 }

7f2cc8b4f9cb4c62c8d593d13e99619cfda7ddf5
Marcelo Junior <marcelojunior.tech@gmail.com>
2025-01-29 11:19:20 -0300

    Implementado "CORS" para o Get_Log

--- webroot/get_log.php
+++ webroot/get_log.php
@@ -1,6 +1,15 @@
 <?php
 include_once __DIR__ . "/../config/scw.php";
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        
+    header("Access-Control-Allow-Origin: * ");
+    header('Access-Control-Allow-Credentials: true');
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    
+    exit(0);
+}
+
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
     header('WWW-Authenticate: Basic realm="SCW"');
     header('HTTP/1.0 401 Unauthorized');
